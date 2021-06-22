beb66a1a0090adcd87fb79c93963df32000431ea
Natanael Lima <taelima1997@gmail.com>
2021-06-21 21:02:49 -0300

    add home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
+}
+
+export default Home;
