b8fa6dda24075504ae244be0391bb5985d1105bc
Natanael Lima <taelima1997@gmail.com>
2021-06-22 23:04:55 -0300

    update main app

--- App.tsx
+++ App.tsx
@@ -1,46 +1,9 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
 
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium,
-} from '@expo-google-fonts/inter';
-import {
-  Rajdhani_400Regular,
-  Rajdhani_500Medium,
-  Rajdhani_700Bold,
-} from '@expo-google-fonts/rajdhani';
+import Main from './src';
 
-import Loading from '@/components/Loading';
-import { theme } from '@/styles/theme';
-import SignIn from './src/screens/SignIn';
-
-const App = (): JSX.Element => {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Rajdhani_400Regular,
-    Rajdhani_500Medium,
-    Rajdhani_700Bold,
-  });
-
-  if (!fontsLoaded) {
-    return <Loading size="large" />;
-  }
-
-  return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-
-      <SignIn />
-    </ThemeProvider>
-  );
+const App = () => {
+  return <Main />;
 };
 
 export default App;
---
+++ src/index.tsx
@@ -0,0 +1,48 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Rajdhani_400Regular,
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
+
+import Loading from '@/components/Loading';
+import { theme } from '@/styles/theme';
+import { Routes } from './routes';
+
+const App = (): JSX.Element => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_400Regular,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading size="large" />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Routes />
+    </ThemeProvider>
+  );
+};
+
+export default App;
