0dd82c5d2331f4e770a5aaf3341263c39b97efe0
Natanael Lima <taelima1997@gmail.com>
2021-06-22 23:03:52 -0300

    udate theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,34 +1,27 @@
 export const theme = {
   colors: {
-    primary: '#E61C44',
-    secondary: '#495BCC',
-    line: '#991F36',
-    green: '#32BD50',
-    heading: '#DDE3F0',
-    body: '#ABB1CC',
-    shape: '#243189',
-    background: '#0E1647',
-  },
+    primary: '#E51C44',
 
-  sizes: {
-    heading: 32,
-    heading2: 20,
-    textLg: 17,
-    text: 15,
-    textSm: 12,
+    secondary100: '#0A1033',
+    secondary90: '#0D133D',
+    secondary80: '#0E1647',
+    secondary70: '#1B2565',
+    secondary60: '#1B2565',
+    secondary50: '#243189',
+    secondary40: '#1D2766',
+    secondary30: '#495BCC',
+
+    overlay: 'rgba(0,0,0,0.7)',
+    highlight: '#ABB1CC',
+    heading: '#DDE3F0',
+    line: '#991F36',
+    on: '#32BD50',
   },
 
   fonts: {
-    primary400: 'Inter_400Regular',
-    primary500: 'Inter_500Medium',
-    secondary400: 'Rajdhani_400Regular',
-    secondary500: 'Rajdhani_500Medium',
-    secondary700: 'Rajdhani_700Bold',
-  },
-
-  border: {
-    radius: 8,
+    title700: 'Rajdhani_700Bold',
+    title500: 'Rajdhani_500Medium',
+    text400: 'Inter_400Regular',
+    text500: 'Inter_500Medium',
   },
 };
-
-export type ThemeType = typeof theme;
