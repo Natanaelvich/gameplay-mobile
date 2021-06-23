1b4079c6cc6a67dc1981314b3ec72b2ec3bb16d7
Natanael Lima <taelima1997@gmail.com>
2021-06-22 23:04:39 -0300

    update home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList } from 'react-native';
 
-import { Text } from 'react-native';
+import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
 
-import { Container } from './styles';
+import { Container, Header, Content, Matches } from './styles';
 
-function Home() {
+export function Home() {
+  const [category, setCategory] = useState('');
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+  ];
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Header>
+        <Profile />
+        <ButtonAdd />
+      </Header>
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+      <Content>
+        <ListHeader title="Partidas agendadas" subtitle="total 6" />
+
+        <Matches
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <ListDivider />}
+        />
+      </Content>
     </Container>
   );
 }
-
-export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,31 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+
+  padding: 0 24px;
+
+  flex-direction: row;
+
+  justify-content: space-between;
+
+  margin-top: ${getStatusBarHeight() + 26}px;
+
+  margin-bottom: 42px;
+`;
+
+export const Content = styled.View`
+  margin-top: 42px;
+`;
+
+export const Matches = styled.FlatList`
+  margin-top: 24px;
+
+  margin-left: 24px;
+`;
