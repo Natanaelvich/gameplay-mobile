bfd8e7f9fb09571ba5c3be77e8f41db434775ab2
Natanael Lima <taelima1997@gmail.com>
2021-06-24 11:39:22 -0300

    fix styles

--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -33,7 +33,6 @@ export const styles = StyleSheet.create({
     width: 64,
     height: 68,
     backgroundColor: theme.colors.secondary50,
-    borderWidth: 1,
     borderRadius: 8,
   },
   field: {
