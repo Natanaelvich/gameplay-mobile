9aa921a0472641ea07c863ab072e755ef36c09f9
Natanael Lima <taelima1997@gmail.com>
2021-06-25 14:55:09 -0300

    add useAuth provider

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,98 @@
+/* eslint-disable import/no-unresolved */
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+import * as AuthSession from 'expo-auth-session';
+
+import {
+  DISCORD_REDIRECT_URI,
+  DISCORD_SCOPE,
+  DISCORD_RESPONSE_TYPE,
+  DISCORD_CLIENT_ID,
+  DISCORD_CDN_IMAGE,
+} from '@env';
+
+import { api } from '../services/api';
+
+type User = {
+  id: string;
+  username: string;
+  firstName: string;
+  avatar: string;
+  email: string;
+  token: string;
+};
+
+type AuthContextData = {
+  user: User;
+  loading: boolean;
+  signIn: () => Promise<void>;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+type AuthorizationResponse = AuthSession.AuthSessionResult & {
+  params: {
+    access_token: string;
+  };
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+  const [loading, setLoading] = useState(false);
+
+  async function signIn() {
+    try {
+      setLoading(true);
+
+      const authUrl = `https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${DISCORD_REDIRECT_URI}&response_type=${DISCORD_RESPONSE_TYPE}&scope=${DISCORD_SCOPE}`;
+
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+
+      if (type === 'success') {
+        api.defaults.headers.authorization = `Bearer ${params.access_token}`;
+
+        const userInfo = await api.get('/users/@me');
+
+        const firstName = userInfo.data.username.split(' ')[0];
+        userInfo.data.avatar = `${DISCORD_CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
+
+        setUser({
+          ...userInfo.data,
+          firstName,
+          token: params.access_token,
+        });
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    } catch {
+      throw new Error('Não foi possível autenticar');
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        signIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
