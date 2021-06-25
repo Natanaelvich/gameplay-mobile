7505752828717af5a075e1ae007b3278f75b4692
Natanael Lima <taelima1997@gmail.com>
2021-06-25 14:54:21 -0300

    add config babel plugin module:react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
@@ -11,6 +11,17 @@ module.exports = function(api) {
           alias: {
             '@': './src/',
           },
+        },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: false
         },
       ],
     ],
