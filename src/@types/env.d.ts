e39c8f72c940bc6fa1f99457126b6b8400d22045
Natanael Lima <taelima1997@gmail.com>
2021-06-25 14:53:26 -0300

    update types

---
+++ src/@types/env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+  export const DISCORD_REDIRECT_URI: string;
+  export const DISCORD_SCOPE: string;
+  export const DISCORD_RESPONSE_TYPE: string;
+  export const DISCORD_CLIENT_ID: string;
+  export const DISCORD_CDN_IMAGE: string;
+}
