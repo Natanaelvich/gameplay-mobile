8a76f5f68a9eff4f360168999f0daf4342aadb96
Natanael Lima <taelima1997@gmail.com>
2021-06-25 23:45:28 -0300

    add new config env

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = function (api) {
           },
         },
       ],
+   "inline-dotenv"
     ],
   };
 };
--- src/@types/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@env' {
-  export const DISCORD_REDIRECT_URI: string;
-  export const DISCORD_SCOPE: string;
-  export const DISCORD_RESPONSE_TYPE: string;
-  export const DISCORD_CLIENT_ID: string;
-  export const DISCORD_CDN_IMAGE: string;
-}
