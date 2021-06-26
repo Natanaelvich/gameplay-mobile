f279666b515670a665bff95467703729d93cd97d
Natanael Lima <taelima1997@gmail.com>
2021-06-25 23:42:53 -0300

    remove all confitn dotenv

--- babel.config.js
+++ babel.config.js
@@ -13,17 +13,6 @@ module.exports = function (api) {
           },
         },
       ],
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: false
-        },
-      ],
     ],
   };
 };
